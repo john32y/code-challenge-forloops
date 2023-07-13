fc9a940c6639bfa45c91a0357d427b1a8584822f
john32y <john.thelronge@gmail.com>
2023-07-12 23:38:25 -0400

    practice for loops WIP

---
+++ Forloops.js
@@ -0,0 +1,64 @@
+// function washDish(Dishes) {
+//   for(let i=0; i<=Dishes; i++) {
+//     console.log(`How many times i told you to wash dishes ${i} times!`)
+//   }
+// };
+//  washDish(20);
+
+//  function moreLoop(numFinger) {
+//   for(let i=1; i<=numFinger; i++) {
+//     console.log(`general hoho said do for loops ${i} times!`)
+//   }
+//  }
+
+//  moreLoop(10);
+
+// function manyLick(Crunch) {
+//   for(let i=1; i<=Crunch; i++) {
+//     console.log(`It takes ${i} licks to get to the center of a toosiepop`)
+//   }
+// }
+// manyLick(3)
+
+// function arrayLoop() { 
+// const prime = [2,3,5,7,11,13,17,19,23,29,31,31,37,41,43,47] 
+//   for (let i = 0; i < prime.length; i++) { 
+//       const result = prime[i]; 
+//       console.log(`all ${result} are prime numbers`); 
+//     }
+// } 
+// arrayLoop(); 
+
+// function arrayLoop() {
+//   const even = [1,2,4,5,6,7,8,9,11,12]
+//   for(let i=0; i<even.length; i++) {
+//     const result = even[i];
+//     if(result % 2 === 0) { // find even thru array
+//       console.log(`${result} is a Even number`)
+//     }
+//   }
+// }
+// arrayLoop()
+
+// function arrayLoop() {
+//   const odd = [1,2,3,5,6,7,9,11,12]
+//   for(let i=0; i<odd.length; i++) {
+//     const result = odd[i];
+//     if(result % 2 !== 0) { //finds odd number
+//       console.log(`${result} is a Odd number`)
+//     }
+//   }
+// }
+// arrayLoop()
+
+function setBlastOff(countDown) {
+  console.log('Blast Off in...');
+  for (let i = countDown; i >= 0; i--) {
+    let countDownText = i + "*".repeat(i);
+    console.log(countDownText);
+    if (i === 0) {
+      console.log('🚀 Blast Off!');
+    }
+  }
+}
+const BlastOff = setBlastOff(10);
